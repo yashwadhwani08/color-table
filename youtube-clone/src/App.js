83725dfa3a13a0e4fa73d3d2f3685d2f7b37aab1
Yash <yashwadhwani08@gmail.com>
2021-12-29 09:17:05 +0530

    Modified default 'App.js' as per the requirement of the project

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/UI/Navbar";
+import Home from "./components/Home";
+import VideoList from "./components/VideoList";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  const submitHandler = (obj) => {
+    let vid;
+    if (videos.length > 0) vid = [...videos, obj];
+    else vid = [obj];
+    setVideos((prev) => vid);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Home submit={submitHandler} /> */}
+      {/* <Home /> */}
+      <Router>
+        <div>
+          <Navbar />
+        </div>
+        <Routes>
+          <Route
+            exact
+            path="/video-list"
+            element={<VideoList videos={videos} />}
+          >
+            {/* <VideoList /> */}
+          </Route>
+
+          <Route exact path="/" element={<Home submit={submitHandler} />}>
+            {/* <Home /> */}
+          </Route>
+          <Route exact path="/home" element={<Home submit={submitHandler} />}>
+            {/* <Home /> */}
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 

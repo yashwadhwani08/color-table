97e55f1b5dc62d1603a395465d0d94a63ce902b6
Yash <yashwadhwani08@gmail.com>
2022-01-13 19:35:41 +0530

    Adding functionality to display Cart Modal when user clicks on  cart icon/button

--- food-order-application/src/components/Layout/Header.js
+++ food-order-application/src/components/Layout/Header.js
@@ -3,15 +3,15 @@ import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Tasty Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       {/* <div className={classes["main-image"]}> */}
-      <div className={classes['main-image']}>
+      <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>

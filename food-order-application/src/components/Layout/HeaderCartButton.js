cd6e0d8a557892e43e92b737823a1d074865b6a2
Yash <yashwadhwani08@gmail.com>
2022-01-14 11:31:19 +0530

    Using conext to update badge value to display total count of different items in Cart

--- food-order-application/src/components/Layout/HeaderCartButton.js
+++ food-order-application/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{ctx.items.length}</span>
     </button>
   );
 };

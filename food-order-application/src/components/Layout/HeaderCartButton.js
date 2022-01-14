414debc7c9ff49e00a479972a9f90dc5a7b4c7f9
Yash <yashwadhwani08@gmail.com>
2022-01-14 12:13:58 +0530

    Modifying Header Cart Button badge to display total items instead of different type of items

--- food-order-application/src/components/Layout/HeaderCartButton.js
+++ food-order-application/src/components/Layout/HeaderCartButton.js
@@ -5,14 +5,17 @@ import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
-  const ctx = useContext(CartContext);
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+    return currentNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{ctx.items.length}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };

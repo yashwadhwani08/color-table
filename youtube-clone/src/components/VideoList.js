90f6c6069669faf537fea5be0ac6b01aeb29cd80
Yash <yashwadhwani08@gmail.com>
2022-01-26 17:33:21 +0530

    Updated the logic to display list of videos and let user generate video link at any time

--- youtube-clone/src/components/VideoList.js
+++ youtube-clone/src/components/VideoList.js
@@ -1,30 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import $ from "jquery";
+import Modal from "./UI/Modal";
 import "./VideoList.css";
 
 const VideoList = (props) => {
-  // const newArr = [...props.videos];
-  const newArr = props.videos;
-  useEffect(() => {
-    var ytApiKey = "AIzaSyCbPEIO-doFWqVa08tb_y3DzfPt2HQChX0";   
-    newArr.forEach((element) => {
-      let vidId = element.youtubeLink.split("v=")[1].substring(0, 11);
-      $.get(
-        "https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" +
-          vidId +
-          "&key=" +
-          ytApiKey,
-        function (data) {
-          element["name"] = data.items[0].snippet.title;
-        }
-      );
-    });
-  }, [newArr]);
-  useEffect(()=>{console.log("Yeah it is working!")},[]);
-
+  const [modalShow, setModalShow] = useState("");
+  const inputRef = useRef();
+  const keyDownHandler = (event, youtubeLink) => {
+    if (event.keyCode === 13) {
+      let value = inputRef.current.value;
+      let timeStampped = youtubeLink + "&t=" + value + "s";
+      navigator.clipboard.writeText(timeStampped);
+      setModalShow(true);
+    }
+  };
   if (props.videos.length > 0) {
-    const listItems = newArr.map((element, i) => {
+    const listItems = props.videos.map((element) => {
       return (
         <tr key={element.id}>
           <td>{element.name}</td>
@@ -39,19 +30,40 @@ const VideoList = (props) => {
               {element.youtubeLink}
             </Link>
           </td>
+          <td>
+            <label htmlFor="timeStamp" style={{ display: "block" }}>
+              Enter the time at which you want to start the video
+            </label>
+            <input
+              type="number"
+              id="timeStamp"
+              onKeyDown={(event) => keyDownHandler(event, element.youtubeLink)}
+              ref={inputRef}
+            />
+          </td>
         </tr>
       );
     });
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>YouTube Video Title</th>
-            <th>YouTube Video Link</th>
-          </tr>
-        </thead>
-        <tbody>{listItems}</tbody>
-      </table>
+      <>
+        {modalShow && (
+          <Modal
+            onClose={() => {
+              setModalShow(false);
+            }}
+          />
+        )}
+        <table>
+          <thead>
+            <tr>
+              <th>YouTube Video Title</th>
+              <th>YouTube Video Link</th>
+              <th>YouTube Permalink</th>
+            </tr>
+          </thead>
+          <tbody>{listItems}</tbody>
+        </table>
+      </>
     );
   } else
     return (

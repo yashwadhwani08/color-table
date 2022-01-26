b371879aba901385c0a5037c50f155e8bb455c09
Yash <yashwadhwani08@gmail.com>
2022-01-26 17:26:26 +0530

    Created modal popup when user generates a YouTube video url at specific time

---
+++ youtube-clone/src/components/UI/Modal.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div style={{ textAlign: "center" }}>
+        The url at the given time has been copied to clipboard!
+      </div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
